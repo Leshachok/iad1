0fc29308bfc20f45354af482ceaf4c1be725bb31
Leshachok <>
2021-09-20 19:34:44 +0000

    importes myclass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import myclass as mc
+from myclass import MyClass
 import averageClass as ac
 import math
 
@@ -15,7 +15,7 @@ ax.grid(b = 1)
 x1 = [0.05, 0.14, 0.16, 0.07, 0.2]
 y1 = [0.91, 0.96, 0.9, 0.7, 0.63]
 
-firstclass = mc.MyClass(ac.AverageClass(x1).Average, ac.AverageClass(y1).Average, '$\clubsuit$', 'green')
+firstclass = MyClass(ac.AverageClass(x1).Average, ac.AverageClass(y1).Average, '$\clubsuit$', 'green')
 
 ax.scatter(x1, y1, c = firstclass.color, s = 400, marker = firstclass.marker)
 
@@ -24,7 +24,7 @@ ax.scatter(x1, y1, c = firstclass.color, s = 400, marker = firstclass.marker)
 x2 = [0.49, 0.34, 0.36, 0.47, 0.52]
 y2 = [0.89, 0.81, 0.67, 0.49, 0.53]
 
-secondclass = mc.MyClass(ac.AverageClass(x2).Average, ac.AverageClass(y2).Average, '$F$', '#46351D')
+secondclass = MyClass(ac.AverageClass(x2).Average, ac.AverageClass(y2).Average, '$F$', '#46351D')
 
 ax.scatter(x2, y2, c = secondclass.color, s = 400, marker = secondclass.marker)
 
@@ -33,7 +33,7 @@ ax.scatter(x2, y2, c = secondclass.color, s = 400, marker = secondclass.marker)
 x3 = [0.62, 0.79, 0.71, 0.78, 0.87]
 y3 = [0.83, 0.92, 0.92, 0.83, 0.92]
 
-thirdclass = mc.MyClass(ac.AverageClass(x3).Average, ac.AverageClass(y3).Average, '*', 'r')
+thirdclass = MyClass(ac.AverageClass(x3).Average, ac.AverageClass(y3).Average, '*', 'r')
 
 ax.scatter(x3, y3, c = thirdclass.color, s = 400, marker = thirdclass.marker)
 
@@ -42,7 +42,7 @@ ax.scatter(x3, y3, c = thirdclass.color, s = 400, marker = thirdclass.marker)
 x4 = [0.55, 0.66, 0.74, 0.89, 0.77]
 y4 = [0.4, 0.32, 0.49, 0.3, 0.2]
 
-fourthclass = mc.MyClass(ac.AverageClass(x4).Average, ac.AverageClass(y4).Average, '1', 'b')
+fourthclass = MyClass(ac.AverageClass(x4).Average, ac.AverageClass(y4).Average, '1', 'b')
 
 ax.scatter(x4, y4, c = fourthclass.color, s = 400, marker = fourthclass.marker)
 
